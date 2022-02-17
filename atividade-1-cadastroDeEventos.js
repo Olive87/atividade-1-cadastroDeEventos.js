6741fe9c36920a202ce9e5e4a21a860030ff87c1
Olive87 <90075986+Olive87@users.noreply.github.com>
2022-02-16 21:49:14 -0300

    first commit

---
+++ atividade-1-cadastroDeEventos.js
@@ -0,0 +1,22 @@
+console . log ("Hello World")
+// Variaveis,cadastro de eventos
+let maiorIdade = 15
+if (maiorIdade >= 18) {
+    consnodeole . log("Participante maior de 18 anos,relizar o cadastro")
+} else {
+  console . log("Paticipante menor de 18 anos, não realizar o cadastro")  
+}
+// Variaveis, número de participantes
+let numeroParticipantes = 120
+if (numeroParticipantes <= 100) {
+    console . log("Número de participantes menor que 100, realizar o cadastro")
+} else {
+   console . log("Número de participantes maior que 100, não realizar o cadastro") 
+}
+// Variáveis, data do evento
+let dataEvento = 16/02/2022
+if (dataEvento > 18/02/2022) {
+    console . log ("Data posterior a data atual, cadastrar, permitir o evento")
+} else {
+  console . log("Cadastro não permitido,data inválida")  
+}
